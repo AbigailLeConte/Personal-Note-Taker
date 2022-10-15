7b6b55ae70d98e1e95e8cd78e6f1c1ac9184bf78
AbigailLeConte <113259320+AbigailLeConte@users.noreply.github.com>
2022-10-15 14:04:28 -0400

    my work for project 11

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const path = require('path');
+// Helper method for generating unique ids
+// const uuid = require('./Assets/index.html');
+const reviews = require('./db/db.json');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET request for reviews
+app.get('/api/notes', (req, res) => {
+  res.status(200).json(noteList);
+});
+
+// POST request to add a review
+// NOTE: Data persistence isn't set up yet, so this will only exist in memory until we implement it
+app.post('/api/notes', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a note`);
+
+  // Destructuring assignment for the items in req.body
+  const { product, review, username } = req.body;
+
+  // If all the required properties are present
+  if (product && review && username) {
+    // Variable for the object we will save
+    const newReview = {
+      product,
+      review,
+      username,
+      review_id: uuid(),
+    };
+
+    const response = {
+      status: 'success',
+      body: newReview,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting notes');
+  }
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
